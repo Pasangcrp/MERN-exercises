4ac5d6cd0a996b7ad816414b544f92797ddb9dc4
Pasang Tshering Sherpa <111878163+Pasangcrp@users.noreply.github.com>
2024-01-02 08:26:09 +0545

    commited exercise 1 in branch exercise1

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App1 from "./exercise1.jsx";
-import App2 from "./exercise2.jsx";
-import App3 from "./exercise3.jsx";
-import App4 from "./exercise4.jsx";
-import App5 from "./quote.jsx";
+import App from "./exercise1.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App1 />
-    <App2 />
-    <App3 />
-    <App4 />
-    <App5 />
+    <App />
   </React.StrictMode>
 );
